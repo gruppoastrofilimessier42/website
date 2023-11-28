47bf636cd5d9fa14dc3ad157255b99c6acaf9587
Gianluca Galloni <gianluca.galloni@stride-it.com>
2023-11-28 21:55:09 +0100

    chore: integrate Firebase, first test with Firestore

---
+++ src/components/header/Mobile.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Mobile() {
+  return (
+    <div className='header-mobile flex' style={{width: '100%', alignItems: 'center', justifyContent: 'center'}}>Mobile version</div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,39 @@
+import Header from '@/components/header/Header';
 import '../styles/global.css';
+import { useEffect } from 'react';
+import { Firebase } from '@/utils/Firebase/Firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
-import type { AppProps } from 'next/app';
+const MyApp = () => {
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+  const initializeFirebase = async () => {
+    const firebase = await Firebase.initialize();
+    console.log(firebase);
+  }
+
+  const initializeDatabase = async () => {
+    const database = await Firebase.initDatabase();
+    console.log(database);
+
+    const querySnapshot = await getDocs(collection(database, 'users'))
+    querySnapshot.forEach((docs) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(docs.id, ' => ', docs.data())
+    })
+  }
+  useEffect(() => {
+    initializeFirebase();
+    initializeDatabase();
+  }, []);
+
+
+  return (
+    <div style={{width: '100vw', height: '100vh'}}>
+      <Header />
+      <p>Ciao Mondo</p>
+      <p>Ciao Mondo numero due</p>
+    </div>
+  );
+};
 
 export default MyApp;
